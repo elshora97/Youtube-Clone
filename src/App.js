d217d55c4295525163d08e2462b903656ab9e4cd
elshora97 <ahmedashrafelshora@gmail.com>
2023-03-19 19:32:30 +0200

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/system";
+import {
+  Navbar,
+  Feed,
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+} from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000 " }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
-
 export default App;
