699e896b54e5c71bb06c59230ecce697a5b0ad83
elshora97 <ahmedashrafelshora@gmail.com>
2023-03-19 22:38:30 +0200

    Video Component

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChannelCard = () => {
+  return <div>asdas</div>;
+};
+
+export default ChannelCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Stack, Box } from "@mui/system";
+import { Video } from "./";
+import { ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>Videux</div>;
+  return (
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => {
+        return (
+          <Box key={index}>
+            {item.id.videoId && <Video video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        );
+      })}
+    </Stack>
+  );
 };
 
 export default Videos;
